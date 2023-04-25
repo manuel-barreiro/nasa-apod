499e9e68f8eb17cb9ad3415a8dd108f3414c61f7
Manuel Barreiro <manuel.barreiro@ing.austral.edu.ar>
2023-04-25 13:31:51 -0300

    functionality done

---
+++ main.js
@@ -0,0 +1,20 @@
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch(){
+  const choice = document.querySelector('input').value
+  console.log(choice)
+  const url = `https://api.nasa.gov/planetary/apod?api_key=ThKFdkOcB0xYVmMs7ZwMeFhzUj34JmdzfxGWLrs6&date=${choice}`
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON  
+      .then(data => {
+        console.log(data)
+        document.querySelector('h2').innerText = data.title
+        document.querySelector('img').src = data.url
+        document.querySelector('h3').innerText = data.explanation
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
+
